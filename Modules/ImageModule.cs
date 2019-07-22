42d4ab99896fbacc1d0593556da21ccd1250f4c6
M3IY0U <timo.nock@stud.h-da.de>
2019-07-22 13:12:09 +0200

    oops

--- Modules/ImageModule.cs
+++ Modules/ImageModule.cs
@@ -14,23 +14,26 @@ namespace Meiyounaise.Modules
     public class ImageModule : BaseCommandModule
     {
         [Command("ir"), Aliases("imagerecognition", "whatsthis")]
-        [Description("Returns information about either an image attachment or an url to an image, also works for the previous message")]
+        [Description(
+            "Returns information about either an image attachment or an url to an image, also works for the previous message")]
         public async Task ImageRecognition(CommandContext ctx, string imageUrl = "")
         {
-            if (imageurl == "")
+            if (imageUrl == "")
             {
                 if (ctx.Message.Attachments.Count != 0)
                 {
-                    imageurl = ctx.Message.Attachments.First().Url;
+                    imageUrl = ctx.Message.Attachments.First().Url;
                 }
                 else
                 {
                     var messages = await ctx.Channel.GetMessagesAsync(2);
-                    imageurl = messages.Last().Attachments.Count != 0 ? messages.Last().Attachments.First().Url : messages.Last().Content;
+                    imageUrl = messages.Last().Attachments.Count != 0
+                        ? messages.Last().Attachments.First().Url
+                        : messages.Last().Content;
                 }
             }
 
-            await Utilities.DownloadAsync(new Uri(imageurl), "image.png");
+            await Utilities.DownloadAsync(new Uri(imageUrl), "image.png");
 
             var client = new HttpClient();
             client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", Utilities.GetKey("recognition"));
@@ -53,19 +56,26 @@ namespace Meiyounaise.Modules
 
             var responseObject = JsonConvert.DeserializeObject<RecognitionResponse>(contentString);
 
+            var tags = responseObject.description.tags.Count == 0
+                ? "None"
+                : string.Join(", ", responseObject.description.tags);
+            var categories = responseObject.categories.Count == 0
+                ? "None"
+                : responseObject.categories.Aggregate("",
+                    (current, category) => current + $"\n{category.name}; Score {Math.Round(category.score, 2)}");
+            var captions = responseObject.description.captions.Count == 0
+                ? "None"
+                : responseObject.description.captions.Aggregate("",
+                    (current, desc) => current + $"\n{desc.text}; Confidence {Math.Round(desc.confidence, 2)}");
+
             var eb = new DiscordEmbedBuilder()
                 .WithColor(new DiscordColor(responseObject.color.accentColor))
                 .WithTitle("Image Analysis")
-                .WithThumbnailUrl(imageurl)
-                .WithDescription(responseObject.description.captions.Aggregate("",
-                    (current, desc) => current + $"\n{desc.text}; Confidence {Math.Round(desc.confidence, 2)}"))
-                .AddField("Categories",
-                    responseObject.categories.Aggregate("",
-                        (current, category) => current + $"\n{category.name}; Score {Math.Round(category.score, 2)}"),
-                    true)
-                .AddField("Tags", string.Join(", ", responseObject.description.tags), true)
-                .AddField("Colors",
-                    $"Foreground Color: {responseObject.color.dominantColorForeground}\nBackground Color: {responseObject.color.dominantColorBackground}\nAccent Color: {responseObject.color.accentColor}",
+                .WithThumbnailUrl(imageUrl)
+                .WithDescription(captions)
+                .AddField("Categories",categories,true)
+                .AddField("Tags", tags, true)
+                .AddField("Colors",$"Foreground Color: {responseObject.color.dominantColorForeground}\nBackground Color: {responseObject.color.dominantColorBackground}\nAccent Color: {responseObject.color.accentColor}",
                     true)
                 .AddField("Dimensions",
                     $"{responseObject.metadata.width} x {responseObject.metadata.height}, Format: {responseObject.metadata.format}",
